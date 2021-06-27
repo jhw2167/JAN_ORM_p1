437dd61d6a7dc32fbac49347d5c82ff72064fdb2
Nathaniel Opsal <nathanopsal@yahoo.com>
2021-06-26 20:39:42 -0500

    added updateAllWhere method to ObjectQuery

--- src/main/java/com/revature/jack/ObjectMapper/ObjectQuery.java
+++ src/main/java/com/revature/jack/ObjectMapper/ObjectQuery.java
@@ -774,6 +774,22 @@ public class ObjectQuery {
 
 	}
 
+	public static void udpateAllWhere (String tableName, String columnName, String oldValue, String newValue)
+			throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		
+		PreparedStatement pstmt;
+		
+		DataSource ds = ObjectMapper.getDs();
+		String query = "UPDATE " + tableName + "SET " + columnName + " = '" + newValue + "' WHERE " + columnName + " = '" + oldValue + "';";
+		try (Connection conn = ds.getConnection();) {
+			pstmt = conn.prepareStatement(query.toString());
+			pstmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static void updateObjectToTable(Object newObj)
 			throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
 		tables = ObjectMapper.getTables();
